ab974c7dfdad99b5ba90e529cbfedc4b06dafd5d
Patrik Vesely <anopheles@alfrid.meta.zcu.cz>
2022-05-03 09:51:19 +0200

    Updated extraction params

--- scripts/run_all_extractions_for_imagelist.sh
+++ scripts/run_all_extractions_for_imagelist.sh
@@ -5,18 +5,18 @@ if [ $# -ne 1 ]
     echo "Use: ./run_all_extractions_for_imagelist.sh BASE_IMAGE_LIST_DIR"
 fi
 
-split -l 100000 --numeric-suffixes $1/imagelist_jpg.txt $1/imagelist_jpg_part.txt.
+split -l 70000 --numeric-suffixes $1/imagelist_jpg.txt $1/imagelist_jpg_part.txt.
 
 mkdir ./scripts/generated 2>/dev/null
 
-for extractor in $'-e \\\\\\\'CLIPExtractor("small")\\\\\\\' --batch_size 64' $'-e \\\\\\\'CLIPExtractor("medium")\\\\\\\' --batch_size 64' \
+for extractor in $'-e \\\\\\\'CLIPExtractor("small")\\\\\\\' --batch_size 128' $'-e \\\\\\\'CLIPExtractor("medium")\\\\\\\' --batch_size 128' \
  $'-e \\\\\\\'EfficientNetExtractor("0")\\\\\\\' --batch_size 256' $'-e \\\\\\\'EfficientNetExtractor("2")\\\\\\\' --batch_size 256' \
  $'-e \\\\\\\'EfficientNetExtractor("4")\\\\\\\' --batch_size 256' $'-e \\\\\\\'EfficientNetExtractor("6")\\\\\\\' --batch_size 256' \
- $'-e \\\\\\\'EfficientNetExtractor("7")\\\\\\\' --batch_size 256' $'-e \\\\\\\'ImageGPTExtractor("small")\\\\\\\' --batch_size 16' \
- $'-e \\\\\\\'ImageGPTExtractor("medium")\\\\\\\' --batch_size 16' \
+ $'-e \\\\\\\'EfficientNetExtractor("7")\\\\\\\' --batch_size 256' $'-e \\\\\\\'ImageGPTExtractor("small")\\\\\\\' --batch_size 128' \
+ $'-e \\\\\\\'ImageGPTExtractor("medium")\\\\\\\' --batch_size 128' \
  $'-e \\\\\\\'ResNetV2Extractor("50")\\\\\\\' --batch_size 256' $'-e \\\\\\\'ResNetV2Extractor("101")\\\\\\\' --batch_size 256' \
  $'-e \\\\\\\'ResNetV2Extractor("152")\\\\\\\' --batch_size 256' \
- $'-e \\\\\\\'ViTExtractor("base")\\\\\\\' --batch_size 1' $'-e \\\\\\\'ViTExtractor("large")\\\\\\\' --batch_size 1' \
+ $'-e \\\\\\\'ViTExtractor("base")\\\\\\\' --batch_size 128' $'-e \\\\\\\'ViTExtractor("large")\\\\\\\' --batch_size 128' \
  $'-e \\\\\\\'W2VVExtractor(networks_path="feature-extractor/models", batch_size=128)\\\\\\\' --batch_size 128'
 do
 	echo "$extractor"
