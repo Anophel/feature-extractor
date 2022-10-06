08c422ef06fd23910b68548d355e30c00bf01921
Patrik Vesely <prtrikvesely@gmail.com>
2022-10-06 10:25:37 +0200

    Bump transformers version

--- scripts/run_extractor_singularity.sh
+++ scripts/run_extractor_singularity.sh
@@ -2,7 +2,7 @@ pip install --no-cache-dir opencv-python-headless==4.5.5.62
 pip install --no-cache-dir torch==1.9.0
 pip install --no-cache-dir matplotlib==3.3.4
 pip install --no-cache-dir alive_progress==2.2.0
-pip install --no-cache-dir transformers==4.16.2
+pip install --no-cache-dir transformers==4.22.0
 pip install --no-cache-dir scikit_image==0.19.1
 pip install --no-cache-dir mxnet-cu112==1.8.0.post0
 pip install --no-cache-dir tensorflow==2.6.0
