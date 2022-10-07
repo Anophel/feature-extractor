a8bea6c9ea36c1ada07d849219c65721904d01ea
Patrik Vesely <prtrikvesely@gmail.com>
2022-10-07 15:15:08 +0200

    Fix filter list

--- create_triplets_v2.py
+++ create_triplets_v2.py
@@ -106,7 +106,7 @@ def main(args):
     print(f"Number of targets: {num_of_targets}")
 
     videos_list = None
-    if "videos_filter" in config and type(config["videos_filter"]) is str and not config["videos_filter"]:
+    if "videos_filter" in config and type(config["videos_filter"]) is str and config["videos_filter"] != "":
         assert os.path.exists(config["videos_filter"])
         with open(config["videos_filter"], "r") as f:
             videos_list = np.array([line.rstrip() for line in f])
